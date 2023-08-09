cb00ff3593e92df6fc999d84305a073192325229
usanaphtal112 <usanaphtal112@gmail.com>
2023-08-09 20:04:55 +0300

    Initial commit

---
+++ src/main-page/index.js
@@ -0,0 +1,52 @@
+import { useState, useEffect, useMemo } from "react";
+import "./main-page.css";
+import Header from "./header";
+import FeaturedHouse from "./featured-house";
+import HouseFilter from "./house-filter";
+import SearchResults from "../search-results";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HouseFromQuery from "../house/HouseFromQuery";
+
+function App() {
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
+
+  return (
+    <Router>
+      <div className="container">
+        <Header subtitle="Providing houses all over the world" />
+        <HouseFilter allHouses={allHouses} />
+
+        <Routes>
+          <Route
+            path="/searchresults/:country"
+            element={<SearchResults allHouses={allHouses} />}
+          />
+          <Route
+            path="/house/:id"
+            element={<HouseFromQuery allHouses={allHouses} />}
+          />
+          <Route path="/" element={<FeaturedHouse house={featuredHouse} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/search-results/search-result-row.js
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import "./search-results.css";
+
+const SearchResultsRow = ({ house }) => {
+  const navigate = useNavigate();
+
+  const setActive = () => {
+    // history.push(`/house/${house.id}`);
+    navigate(`/house/${house.id}`);
+  };
+
+  return (
+    <tr onClick={setActive}>
+      <td>{house.address}</td>
+      <td>{house.price}</td>
+      <td>{house.likes}</td>
+    </tr>
+  );
+};
+
+export default SearchResultsRow;
